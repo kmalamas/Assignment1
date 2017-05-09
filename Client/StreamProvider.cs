17cdf1d6e71c9122185c77ba713082e88c091068
kmalamas <kyrmalamas@gmail.com>
2017-05-10 00:10:37 +0200

    Added StreamProvider class to handle creation of streams from tweeterinvi

---
+++ Client/StreamProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+
+namespace Client
+{
+    public static class StreamProvider
+    {
+        public static void CreateFilteredStream(string filter)
+        {
+            var stream = Stream.CreateFilteredStream();
+            stream.AddTrack(filter);
+            stream.MatchingTweetReceived += (sender, arg) =>
+            {
+                Console.WriteLine("A tweet containing '"+ filter+"' has been found; the tweet is '" + arg.Tweet + "'");
+            };
+            stream.StartStreamMatchingAllConditions();
+        }
+
+    }
+}

f04e97b96346e4080e632686dfc93d80175a43d0
kmalamas <kyrmalamas@gmail.com>
2017-05-09 23:54:31 +0200

    Added AuthenticationHandler class to handle authentication using twitterinvi library

---
+++ Client/AuthenticationHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+
+namespace Client
+{
+    public class AuthenticationHandler
+    {
+        private string consumerKey;
+        private string consumerSecret;
+        private string accessToken;
+        private string accessTokenSecret;
+
+        public void setCredentials()
+        {
+            // Set up your credentials (https://apps.twitter.com)
+            Auth.SetUserCredentials(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+        }
+        public AuthenticationHandler(string consumerKey, string consumerSecret, string accessToken, string accessTokenSecret)
+        {
+            this.consumerKey = consumerKey;
+            this.consumerSecret = consumerSecret;
+            this.accessToken = accessToken;
+            this.accessTokenSecret = accessTokenSecret;
+        }
+
+    }
+}

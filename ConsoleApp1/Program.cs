eecdbc1944e0cc3f433d0bc8d5cc87f5d6ea7746
kmalamas <kyrmalamas@gmail.com>
2017-05-10 00:11:55 +0200

    simple console app that prompts for a term to use as a filter for the stream

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Client;
+
 
 namespace ConsoleApp1
 {
@@ -10,6 +12,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string consumerKey = "jUKRj9Wd98fawW8fpzddvJDIe";
+            string consumerSecret = "zwO9V1Bf6cHyQrilVpkVBfckpHEgbLJ8lgMh7pXcCHrNpyqZw7";
+            string accessToken = "299655316-Aw44Yhlv9YRQQvKNzu8lg6yMzvnlqBmcCzBXqiM1";
+            string accessTokenSecret = "9R8zwZqnKN1aROEBZ6ALKAAbH9TUibj23i1DTQiW3kb8A";
+            AuthenticationHandler auth = new AuthenticationHandler(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+            auth.setCredentials();
+            Console.WriteLine("Please input filter term to start stream");
+            string filter = Console.ReadLine();
+            if (!string.IsNullOrEmpty(filter))
+            {
+                StreamProvider.CreateFilteredStream(filter);
+            }
+
         }
     }
 }

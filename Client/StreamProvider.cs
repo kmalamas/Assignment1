d824016f3a65d53e7dfac2b91e7a50bbedae152a
kmalamas <kyrmalamas@gmail.com>
2017-05-11 01:10:00 +0200

    getting images url now and testing tweets/hour rate

--- Client/StreamProvider.cs
+++ Client/StreamProvider.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using Tweetinvi;
 
 namespace Client
@@ -11,13 +12,30 @@ namespace Client
     {
         public static void CreateFilteredStream(string filter)
         {
+            int counter = 0;
+            
             var stream = Stream.CreateFilteredStream();
             stream.AddTrack(filter);
+            DateTime then = DateTime.Now; // the start time
             stream.MatchingTweetReceived += (sender, arg) =>
             {
-                Console.WriteLine("A tweet containing '"+ filter+"' has been found; the tweet is '" + arg.Tweet + "'");
+                counter++;
+                Console.WriteLine(CalculateTweetsPerHour(counter, then));
+                if (arg.Tweet.Entities.Medias.Count>0)
+                    Console.WriteLine("A tweet " + arg.Tweet.Entities.Medias.First().MediaURL + "'");
+                //Console.WriteLine("A tweet containing '"+ filter+"' has been found; the tweet is '" + arg.Tweet + "'");
             };
+
             stream.StartStreamMatchingAllConditions();
+           
+           
+        }
+
+        public static double CalculateTweetsPerHour(int tweetsCounter, DateTime then)
+        {
+            double elapsedSeconds = (DateTime.Now - then).TotalSeconds;
+            double rate = (tweetsCounter * 3600) / elapsedSeconds.;
+            return rate;
         }
 
     }
